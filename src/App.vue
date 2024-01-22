f4f84289a2880d3badaf42545f481519df6180ee
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-01-22 17:22:06 +0100

    working axios call

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
-
+    <h1>hello vue</h1>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { store } from './data/store.js';
 export default {
-
+  name: "App",
+  components: {},
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getApiTest() {
+      axios.get(this.store.apiUrl + "projects").then((res) => {
+        console.log(res.data);
+      });
+    }
+  },
+  mounted() {
+    this.getApiTest();
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "./assets/styles/partials/variables"
+</style>
